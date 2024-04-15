26e5bbeb5f542c45db6a3e5161996d9af02c204d
Milos Vlku <milos.vlku@vegait.rs>
2024-04-15 16:51:10 +0200

    Application properties

--- project/backend/eschool/src/main/java/ftn/eschool/entities/Person.java
+++ project/backend/eschool/src/main/java/ftn/eschool/entities/Person.java
@@ -1,12 +1,22 @@
 package ftn.eschool.entities;
 
 import ftn.eschool.entities.enums.Gender;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Entity
+@Table(name = "person")
 public abstract class Person {
-    private UUID ID;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
     private Gender gender;
     private String firstName;
     private String lastName;
