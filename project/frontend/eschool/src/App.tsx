ab20b9cb7a458a954c9cfb8269c84f8ecf50c312
milos.vlku.learning <milos.vlku.learning@gmail.com>
2025-02-21 15:47:31 +0100

    Initial commit Vite, Tailwind, Shadcn

---
+++ project/frontend/eschool/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from './components/ui/button';
+
+function App() {
+
+
+  return (
+    <>
+        <h1 className='text-5xl text-center p-16'>Test</h1>
+        <Button variant="default">Button</Button>
+        <Button variant="milos">Button</Button>
+    </>
+  )
+}
+
+export default App
